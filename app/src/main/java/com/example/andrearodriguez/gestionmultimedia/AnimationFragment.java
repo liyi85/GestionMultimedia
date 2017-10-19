e7939e1b0027cfa86f9f69de066d766570fdab6c
Andrea Rodriguez <liyi85@gmail.com>
2017-10-18 19:09:25 -0500

    Initial commit menu lateral, opciones, primer fragment

---
+++ app/src/main/java/com/example/andrearodriguez/gestionmultimedia/AnimationFragment.java
@@ -0,0 +1,35 @@
+package com.example.andrearodriguez.gestionmultimedia;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AnimationFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.app_bar_main, container, false);
+
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if (getActivity() instanceof MainActivity) {
+            MainActivity activity = (MainActivity) getActivity();
+            activity.updateView(getString(R.string.titulo), (getString(R.string.animacion)));
+            activity.navigationView.setCheckedItem(R.id.nav_animation);
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/andrearodriguez/gestionmultimedia/MainActivity.java
@@ -0,0 +1,97 @@
+package com.example.andrearodriguez.gestionmultimedia;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity
+                        implements NavigationView.OnNavigationItemSelectedListener{
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    LinearLayout linearLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.container, new AnimationFragment())
+                .commit();
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        if (navigationView != null)
+            navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(navigationView))
+            drawerLayout.closeDrawer(GravityCompat.START);
+        else
+            super.onBackPressed();
+    }
+
+    public void updateView(String title, String subtitle) {
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        linearLayout = (LinearLayout) findViewById(R.id.header);
+
+        if (toolbar != null)
+            toolbar.setTitle(title);
+        toolbar.setSubtitle(subtitle);
+        setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle toogle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.open_drawer, R.string.close_drawer);
+        drawerLayout.addDrawerListener(toogle);
+        toogle.syncState();
+
+    }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        Fragment fragment = null;
+
+        switch (id) {
+            case R.id.nav_animation:
+                fragment = new AnimationFragment();
+                break;
+            case R.id.nav_graficos:
+                fragment = new GraficosFragment();
+                break;
+
+            case R.id.nav_imagenes:
+                //fragment = new ImagenFragment();
+                break;
+
+            case R.id.nav_audio:
+                //fragment = new AudioFragment();
+                break;
+
+            case R.id.nav_video:
+                //fragment = new VideoFragment();
+                break;
+        }
+        if (fragment != null)
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, fragment)
+                    .addToBackStack(null)
+                    .commit();
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
